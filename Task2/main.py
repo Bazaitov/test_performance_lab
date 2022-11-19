97a7560c59863d0e8a2ec70be3d8ad4dfa7c4560
Bazaitov <bim090197@gmail.com>
2022-11-19 12:48:43 +0300

    Update main.py

--- Task2/main.py
+++ Task2/main.py
@@ -18,14 +18,15 @@ if __name__ == '__main__':
     cData[0] = cData[0].split()
     cData[0] = list(map(float, cData[0]))
     cData[1] = list(map(float, cData[1]))
+    rezult = ""
 
     for i in range(len(pData)):
         pData[i] = pData[i].split()
         pData[i] = list(map(float, pData[i]))
         if (cData[0][0] - pData[i][0]) ** 2 + (cData[0][1] - pData[i][1]) ** 2 == cData[1][0] ** 2:
-            pData[i] = 0
-        elif (1 - pData[i][0]) ** 2 + (1 - pData[i][1]) ** 2 < cData[1][0] ** 2:
-            pData[i] = 1
+            rezult = rezult + "0\n"
+        elif (1 - pData[i][0]) ** 2 + (1 - pData[i][1]) ** 2 < 25:
+            rezult = rezult + "1\n"
         else:
-            pData[i] = 2
-    print(pData)
+            rezult = rezult + "2\n"
+    print(rezult)
