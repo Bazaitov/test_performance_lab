bd9ca0c3ac09192a864d86bbebea9ce53c142b5e
Bazaitov <bim090197@gmail.com>
2022-11-28 20:35:40 +0300

    Update main.py

--- Task1/main.py
+++ Task1/main.py
@@ -1,5 +1,19 @@
+import sys
+import argparse as argp
+
+
+def createParser():
+    parser = argp.ArgumentParser()
+    parser.add_argument('-n', '--number', required=True)
+    parser.add_argument('-m', required=True)
+    return parser
+
+
 if __name__ == '__main__':
-    n, m = map(int, input().split())
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    n = int(namespace.number)
+    m = int(namespace.m)
 
     i = 1
     while True:
@@ -8,3 +22,4 @@ if __name__ == '__main__':
         if i == 1:
             break
     print()
+
